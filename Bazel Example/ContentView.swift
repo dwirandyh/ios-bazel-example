38cfd02cc8ae61a2cfa8ca0aec01275921995426
Dwi Randy H <dwirandyherdinanto@gmail.com>
2021-11-21 15:49:46 +0700

    feat(project): Init project

---
+++ Bazel Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bazel Example
+//
+//  Created by Dwi Randy H on 21/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
